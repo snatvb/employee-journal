08a86c3f84c13f24f04a5024a76feefa17244984
Andrew Brghton <snatlip48@gmail.com>
2020-05-18 02:09:49 +0300

    Added calendar month days display

--- src/Helpers/DateTime.elm
+++ src/Helpers/DateTime.elm
@@ -1,6 +1,7 @@
-module Helpers.DateTime exposing (toString)
+module Helpers.DateTime exposing (getDaysInDate, getDaysInMonth, toString)
 
 import Date
+import Time exposing (Month(..))
 
 
 asTextWithPad : (Date.Date -> Int) -> Date.Date -> String
@@ -18,3 +19,59 @@ toString date =
         ++ asTextWithPad Date.monthNumber date
         ++ "."
         ++ asTextWithPad Date.year date
+
+
+getDaysInFeb : Int -> Int
+getDaysInFeb year =
+    if year // 4 == 0 then
+        29
+
+    else
+        28
+
+
+getDaysInMonth : Month -> Int -> Int
+getDaysInMonth month year =
+    case month of
+        Jan ->
+            31
+
+        Feb ->
+            getDaysInFeb year
+
+        Mar ->
+            31
+
+        Apr ->
+            30
+
+        May ->
+            31
+
+        Jun ->
+            30
+
+        Jul ->
+            31
+
+        Aug ->
+            31
+
+        Sep ->
+            30
+
+        Oct ->
+            31
+
+        Nov ->
+            30
+
+        Dec ->
+            31
+
+
+getDaysInDate : Date.Date -> Int
+getDaysInDate date =
+    getDaysInMonth
+        (Date.month date)
+        (Date.year date)
