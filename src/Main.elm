5a7d7c790e1c1e0f8c488c803774dbad0c3b4f8c
Andrew Brghton <snatlip48@gmail.com>
2020-05-22 19:28:33 +0300

    Moved updates to another file

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,13 @@
 module Main exposing (main)
 
 import Action exposing (Action)
-import Action.Store
-import Browser exposing (Document, UrlRequest)
+import Browser exposing (Document)
 import Browser.Navigation as Navigation
-import Helpers exposing (fixPathInUrl)
 import Model exposing (Model)
 import Router exposing (handleUrl)
 import Store exposing (initStore)
-import Url as URL exposing (Url)
-import View
+import Update exposing (update)
+import Url exposing (Url)
 import View.Employee
 import View.Home
 import View.NewEmployee
@@ -28,88 +26,7 @@ subscriptions model =
             View.NewFeature.subscriptions submodel
 
         _ ->
-          Sub.none
-
-
-
-updateStore : Action.Store.Action -> Model -> ( Model, Cmd Action )
-updateStore action model =
-    let
-        ( newStore, cmd ) =
-            Store.update action model.store
-    in
-    ( { model | store = newStore }, cmd )
-
-
-update : Action -> Model -> ( Model, Cmd Action )
-update action model =
-    case action of
-        Action.None ->
-            ( model, Cmd.none )
-
-        Action.Batch actions ->
-            List.foldl handleBatchActions ( model, Cmd.none ) actions
-
-        Action.UrlChanged url ->
-            handleUrl <| Store.updateUrl url model.store
-
-        Action.LinkClicked request ->
-            updateLink request model
-
-        Action.Redirect request ->
-            updateLink request model
-
-        Action.RedirectAfterAction request actionAfterRedirect ->
-            updateByRedirectAction actionAfterRedirect <|
-                updateLink request model
-
-        Action.Store storeAction ->
-            updateStore storeAction model
-
-        Action.Views viewsAction ->
-            View.update viewsAction model
-
-
-updateUrl : Url -> Model -> Model
-updateUrl url model =
-    { model
-        | store =
-            Store.updateUrl url model.store
-    }
-
-
-handleBatchActions : Action -> ( Model, Cmd Action ) -> ( Model, Cmd Action )
-handleBatchActions action ( model, cmds ) =
-    let
-        ( newModel, newCmds ) =
-            update action model
-    in
-    ( newModel, Cmd.batch [newCmds, cmds] )
-
-
-updateByRedirectAction : Action -> ( Model, Cmd Action ) -> ( Model, Cmd Action )
-updateByRedirectAction action ( model, cmds ) =
-    let
-        ( newModel, newCmds ) =
-            update action model
-    in
-    ( newModel, Cmd.batch [ newCmds, cmds ] )
-
-
-updateLink : UrlRequest -> Model -> ( Model, Cmd Action )
-updateLink request model =
-    case request of
-        Browser.Internal url ->
-            let
-                fixedUrl =
-                    fixPathInUrl url
-            in
-            ( updateUrl fixedUrl model, Navigation.pushUrl model.store.navigationKey (URL.toString fixedUrl) )
-
-        Browser.External url ->
-            ( model
-            , Navigation.load url
-            )
+            Sub.none
 
 
 render : Model -> Document Action
